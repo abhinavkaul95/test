1b4ac3c35bc93aa451c5cc12f17e04453857617f
abhinavkaul95 <abhinavkaul95@gmail.com>
2021-08-22 09:35:46 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import time
 from datetime import datetime as dt
 with open("test.txt", "a+") as f:
     timestr = dt.now().strftime("%H:%M:%S")
-    f.write(timestr + " Here!\n")
+    f.write(timestr + " Hi here!\n")
     time.sleep(1)
